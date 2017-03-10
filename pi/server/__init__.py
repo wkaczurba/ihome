1d2040672a0b8b6b997d4910ac9de200a9385755
wkaczurba <witold.kaczurba@gmail.com>
2017-03-10 06:04:25 +0000

    checking os for pi

--- pi/server/__init__.py
+++ pi/server/__init__.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 SERVER_BOOT = "http://127.0.0.1:8080"
 SERVER_STANDALONE = "http://127.0.0.1:8080/ihome" 
@@ -6,10 +7,12 @@ SERVER_STANDALONE = "http://127.0.0.1:8080/ihome"
 #server = SERVER_BOOT
 
 def getName():
-    #possible = { 'boot':"http://127.0.0.1:8080", 'statusIhome':"http://127.0.0.1:8080/ihome" }
-    
-    
-    possible = ["http://127.0.0.1:8080", "http://127.0.0.1:8080/ihome"]
+
+    if ("raspberrypi" in os.uname()):
+        possible = ["http://192.168.0.12:8080", "http://192.168.0.12:8080/ihome"]
+    else:
+        possible = ["http://192.168.0.100:8080", "http://192.168.0.100:8080/ihome"]
+
     statuses = {}
     
     for x in possible:
